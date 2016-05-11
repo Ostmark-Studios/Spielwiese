b2ba1f1789504db9dd203ef9a8b7a2e933597d0d
IM Sandmann <Ostmark-Studios@users.noreply.github.com>
2016-05-11 17:48:03 +0200

    Create index.pl

---
+++ index.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+# Symbolisches Hauptprogramm, das mit den Attributen 755 (rwx r-x r-x) unter Linux gesetzt werden möchte
+# chmod 755 index.pl
+
+# Modul-/Bibliothekeneinbindung
+use strict;
+
+# Formelle Mustervorlage für das Grundverständnis
+
+1;
